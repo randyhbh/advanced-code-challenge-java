299a5f44b70e507428ac73c0060df21653b86d5e
Randy Héctor Bartumeu Huergo <randyhbh91@gmail.com>
2023-12-04 22:13:22 +0100

    add booking controller

---
+++ code-challenge/src/main/java/com/statista/code/challenge/api/http/v1/BookingController.java
@@ -0,0 +1,17 @@
+package com.statista.code.challenge.api.http.v1;
+
+import com.statista.code.challenge.api.http.v1.requests.CreateBookingRequest;
+import com.statista.code.challenge.api.http.v1.responses.BookingResponse;
+import com.statista.code.challenge.usecases.booking.create.CreateBookingCommand;
+import com.statista.code.challenge.usecases.booking.create.CreateBookingUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/booking-service/booking")
+public class BookingController {
+
+}
