471ef559419f79c4ca711f36deab965444a188a4
Randy Héctor Bartumeu Huergo <randyhbh91@gmail.com>
2023-12-04 22:33:00 +0100

    add logger  config

---
+++ code-challenge/src/main/java/com/statista/code/challenge/config/LoggingConfig.java
@@ -0,0 +1,34 @@
+package com.statista.code.challenge.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.core.MethodParameter;
+
+import java.lang.reflect.Field;
+
+@Configuration
+public class LoggingConfig {
+
+    @Bean
+    @Scope("prototype")
+    public Logger logger(InjectionPoint injectionPoint) {
+        MethodParameter methodParameter = injectionPoint.getMethodParameter();
+        Field field = injectionPoint.getField();
+
+        if (methodParameter != null) {
+            var targetClass = methodParameter.getContainingClass();
+            return LoggerFactory.getLogger(targetClass);
+        }
+
+        if (field != null) {
+            var targetClass = field.getDeclaringClass();
+            return LoggerFactory.getLogger(targetClass);
+        }
+
+        throw new RuntimeException("Could not produce all the required Loggers");
+    }
+}
